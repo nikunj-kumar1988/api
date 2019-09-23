3027634587f84ef16b548808ea47c10649a0b7d7
Nikunj Kumar <nikunj.kumar@mindtree.com>
2019-09-23 23:22:31 +0530

    api

--- spec_tests/getboards.api.spec.js
+++ spec_tests/getboards.api.spec.js
@@ -12,12 +12,15 @@ const joi = require('@hapi/joi');
 describe('Get boards by id ', () => {
 
     let baseURL = config.get("app.baseurl");
+    console.log(baseURL);
     it('should return the boards by id ', () => {
         request.get(baseURL)
             .get('/1/boards/5d7688345107172fd1cbd442?key=46a6c26def8977d0ef84d6a9693db22c&token=ff857c2f92c951abf3d61ed4060de32cd418491dcea72847959c77cd05f30db6')
             .set('Accept', 'application/json')
             .end((err, res) => {
                 if (err) done.fail(err);
+
+                console.log("inside methiod");
                 expect(res.status).toBe(200);
                 expect(Object.keys(res.body).length).toBeGreaterThan(0);
             });
